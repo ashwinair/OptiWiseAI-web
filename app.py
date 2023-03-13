e3719aa2577c293e569c961680e3d7dc0619f00a
Ashwin Nair <ashwinnair0007@gmail.com>
2023-03-13 10:05:12 +0000

    updated

--- app.py
+++ app.py
@@ -1,40 +1,52 @@
-2
-3
-4
-5
-6
-7
-8
-9
-10
-11
-12
-13
-14
-15
-16
-17
-18
-19
-20
-	
-# how to use css in python_ flask
-# flask render_template example
- 
-from flask import Flask, render_template
- 
-# WSGI Application
-# Provide template folder name
-# The default folder name should be "templates" else need to mention custom folder name
+
+from flask import Flask, render_template, request
+from product_details import ProductDetails
+#from optiwise.chat_sonic import ChatSonic
+from optiwise.chat_gpt import ChatGPT
+
 app = Flask(__name__, template_folder='template', static_folder='static')
- 
+
 # @app.route('/')
 # def welcome():
 #     return "This is the home page of Flask Application"
- 
+
 @app.route('/')
 def index():
     return render_template('index.html')
- 
-if __name__=='__main__':
-    app.run(debug = True)
+
+@app.route('/get_data', methods=['POST'])#, methods=['POST']
+def perform_url_actions():git st
+
+    url = request.form['url']
+    pro_details = ProductDetails(url)
+    
+    list_of_info = pro_details.get_information_section()
+
+    #product_name = product_details.get_product_name_and_type()
+    product_type = "face wash"
+    ingredients = list_of_info.get('Ingredients')
+    #print(list_of_info)
+    safety_information = list_of_info.get('Safety Information')
+    directions = list_of_info.get('Directions')
+    res = {"good_ingredients" : ingredients}
+    if len(ingredients) <= 1:
+        #return not enough details :(
+        print(ingredients)
+        ingredients = "Not enough data available"
+        print(ingredients)
+    else:
+        #optiWise = ChatSonic(product_type, ingredients)
+        print("here goes the money")
+        optiWise = ChatGPT(product_type, ingredients)
+        result = optiWise.generate_ingredients_classification()
+        #response = optiWise.generate_response() #it will return dict 
+        #have to refine data and process nicely
+        # print(safety_information)
+        # print(directions)
+        # print(response)
+        return result
+    return res
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
--- static/script.js
+++ static/script.js
@@ -1,38 +1,75 @@
-const productNameValue = productName.value;
-const ingredientsValue = ingredients.value;
+const form = document.getElementById('url-form');
+    const goodListElement = document.getElementById('good-list');
+	const badListElement = document.getElementById('bad-list');
+	const noteElement = document.getElementById('note');
+    form.addEventListener('submit', event => {
+      event.preventDefault();
+      const urlInput = document.getElementById('url-input').value;
+      fetch('/get_data', {
+        method: 'POST',
+        body: new URLSearchParams({
+          'url': urlInput
+        })
+      })
+      .then(response => response.json())
+      .then(data => {
+        // create an HTML string to display the data
+        console.log('here is the error')
+        let html = '';
+        for (const [key, value] of Object.entries(data)) {
+			if (key === 'good_ingredients'){
+				goodListElement.innerHTML = `<p><strong>${key}:</strong> ${value}</p>`
+			}
+			else if (key === 'bad_ingredients'){
+				badListElement.innerHTML = `<p><strong>${key}:</strong> ${value}</p>`
+			}
+			else if (key === 'information'){
+				noteElement.innerHTML = `<p><strong>${key}:</strong> ${value}</p>`
+			}
+      // console.log(data['token_size'])
+         // html += ;
+        }
+        // display the HTML string in the result element
+         //= html;
+      })
+      .catch(error => console.error(error));
+    });
 
-function validateForm() {
-	const productName = document.getElementById("product-name");
-	const ingredients = document.getElementById("ingredients");
+// const productNameValue = productName.value;
+// const ingredientsValue = ingredients.value;
 
-	if (productName.value.trim() == "" || ingredients.value.trim() == "") {
-		if (productName.value.trim() == "") {
-			productName.classList.add("error");
-		} else {
-			productName.classList.remove("error");
-		}
+// function validateForm() {
+// 	const productName = document.getElementById("product-name");
+// 	const ingredients = document.getElementById("ingredients");
 
-		if (ingredients.value.trim() == "") {
-			ingredients.classList.add("error");
-		} else {
-			ingredients.classList.remove("error");
-		}
+// 	if (productName.value.trim() == "" || ingredients.value.trim() == "") {
+// 		if (productName.value.trim() == "") {
+// 			productName.classList.add("error");
+// 		} else {
+// 			productName.classList.remove("error");
+// 		}
 
-		return false;
-	} else {
-		productName.classList.remove("error");
-		ingredients.classList.remove("error");
+// 		if (ingredients.value.trim() == "") {
+// 			ingredients.classList.add("error");
+// 		} else {
+// 			ingredients.classList.remove("error");
+// 		}
 
-		console.log("Product Name:", productNameValue);
-		console.log("Ingredients:", ingredientsValue);
+// 		return false;
+// 	} else {
+// 		productName.classList.remove("error");
+// 		ingredients.classList.remove("error");
 
-		return true;
-	}
-}
+// 		console.log("Product Name:", productNameValue);
+// 		console.log("Ingredients:", ingredientsValue);
 
-function SubmitForm(){
-	if(validateForm()){
-		productName.value;
-		ingredients.value;
-	}
-}
+// 		return true;
+// 	}
+// }
+
+// function SubmitForm(){
+// 	if(validateForm()){
+// 		productName.value;
+// 		ingredients.value;
+// 	}
+// }
